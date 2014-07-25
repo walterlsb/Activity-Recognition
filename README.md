b987c04ff0b707f70fa428f6068ad568902bfd10
Walter Burrough <walterlsb@users.noreply.github.com>
2014-07-24 20:15:14 -0500

    Added readme.md and cross reference to run_analyis.r

--- README.md
+++ README.md
@@ -2,3 +2,36 @@ Activity-Recognition
 ====================
 
 Submission for Coursera Getting and Cleaning Data class
+
+
+How the code works
+====================
+1.   The script reads the variable labels from features.txt and activity_labels.txt
+2.   Read the "x tables" (x_train.txt and x_test.txt)
+3.   Apply the labels from activity_labels.txt to the x tables as variable names
+4.   The subject tables (subject_train.txt and subject_test.txt) are simply the subjectId associated with the
+     x table features. Read the one column tables and append them directly to the x tables. Label the
+     feature as SubjectId
+5.   The "y tables" list the activity associated with each set of features in the x tables. Read the
+     y tables, name the feature actId and append it to the relevant x table.
+6.   Now we have a x table that looks something like this:
+	|actId|SubjectId|tBodyAcc-mean()-X|...|
+	|-----|---------|-----------------|---|
+	|1    |12       {0.2160924        |...|
+   We will merge the activity_labels data on the actId key to add text activity labels and we get:
+	|actId|Activity|SubjectId|tBodyAcc-mean()-X|...|
+	|-----|--------|---------|-----------------|---|
+	|1    |WALKING |12       {0.2160924        |...|
+7.   Now both x tables are complete so we can join them with rbind. 
+8.  Because only features labeled *mean* and *standard deviation* are wanted in the final data, the script 
+   uses the labels from features.txt and searches it with a regular expression for any labels including *mean* 
+   or *std*. These are stored in *vars* which is coerced into a character vector and feature labels *Activity*
+    and *SubjectID* are appended to it.
+9.  The columns labeled in *vars* are selected into data frame *dataset*.
+10.  To rename the features with descriptive names, the script exports the current feature names into an Excel spreadsheet where the user adds another column with their own names. Then reimport it and the script renames the columns in *dataset*. [See codebook for naming system]
+11.  Average values grouped by Subject and Activity are calculated using the *aggregate* function which outputs the results to *SummaryData* which looks like this
+	|Group.1|Group.2|Activity|SubjectId|tBodyAcc-mean()-X|...|
+	|-------|-------|--------|---------|-----------------|---|
+	|LAYING |1      |NA      |        1|-0.928056469     |...|
+12.  Finally we remove columns 2,3 and rename the activity text feature and export to csv
+
--- run_analysis.r
+++ run_analysis.r
@@ -1,12 +1,12 @@
 
-## read the variable labels and activity labels
+## 1 - read the variable labels and activity labels
 labels<-read.table("features.txt")
 activity_labels<-read.table("activity_labels.txt")
 
 
 
 #########################################
-## x-tables
+## 2,3 - x-tables
 ## read the x_train table and label the columns with meaningful names
 x_train<-read.table("./train/x_train.txt")
 names(x_train)<-labels[,2]
@@ -20,6 +20,7 @@ names(x_test)<-labels[,2]
 
 
 #########################################
+## 4 - subject tables
 ## subject tables are a list of subjects one for each record in the x tables
 ## append column at front of data table with subject ID
 ##
@@ -35,52 +36,57 @@ names(x_train)[1]<-"SubjectID"
 
 
 #########################################
+## 5 - y tables
 ## y tables hold an id to activities
-## First merge the y tables with text labels
+## First copy the activityIds to the x tables
 
 y_test<-read.table("./test/y_test.txt")
 y_train<-read.table("./train/y_train.txt")
 
 
-## merge the activity with the y tables to give a list of activity observations to add to the x_test datafile
+##  name the column and add it to the x table
 names(y_test)[1]<-"actId"
 names(y_train)[1]<-"actId"
-names(activity_labels)<-c("actId","Activity")
 
 x_test<-cbind("actId"=y_test,x_test)
 x_train<-cbind("actId"=y_train,x_train)
+
+
+## 6 - merge the activity labels
+names(activity_labels)<-c("actId","Activity")
 
 x_test<-merge(activity_labels,x_test,x.lab="actId",y.lab="actId")
 x_train<-merge(activity_labels,x_train,x.lab="actId",y.lab="actId")
 
 
-## label and join the two data sets together
-x_train<-cbind("Dataset"="training",x_train)
-x_test<-cbind("Dataset"="test",x_test)
+## 7 -  join the two data sets together
 
 dataset<-rbind(x_train,x_test)
 
+## 8 - subset only the observations we want
 ## indentify the variables including mean and std in their labels
 meanVars<-labels[grep("mean",labels$V2),]
 stdVars<-labels[grep("std",labels$V2),]
 vars<-rbind(stdVars,meanVars)[,2]
 vars<-as.character(vars)
-vars<-c("Dataset","Activity","SubjectID",vars)
+vars<-c("Activity","SubjectID",vars)
 
-## subset the dataset with the variables
+## 9 - subset the dataset with the variables
 dataset<-dataset[vars]
 
-## Rename the variables with "descriptive" names.
+## 10 - Rename the variables with "descriptive" names.
 ## export current variable names into Excel, edit them and resave and reload
 library(xlsx)
 write.xlsx(vars,"vars.xlsx")
 ## edit the excel file
 newvars<-read.xlsx("vars.xlsx",1)
-> names(dataset)<-newvars[,3]
+names(dataset)<-newvars[,3]
 
 ##############################################
-## calculate mean for each combination of Activity and Subject
+## 11 - calculate mean for each combination of Activity and Subject
 SummaryData<-aggregate(dataset,list(dataset$Activity,dataset$SubjectID),FUN=mean)
 
 ## clean up the data to return (clean out a few columns)
-SummaryData<-SummaryData[,c(1,5:84)]
+SummaryData<-SummaryData[,c(1,4:83)]
+names(SummaryData)[1]<-"Activity"
+write.csv(SummaryData,"AverageBySubjectActivity.csv")
